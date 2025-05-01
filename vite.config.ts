6be0ee683baa21903f69605aa618a2ffd3d0410e
Suyash Dashputre <suyashdashputre@gmail.com>
2025-05-02 01:54:28 +0530

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { fileURLToPath, URL } from 'url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -38,7 +37,10 @@ export default defineConfig({
   })],
   resolve: {
     alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-}
+      '@/components': '/src/components',
+      '@/utils': '/src/utils',
+      '@/hooks': '/src/hooks',
+      '@/lib': '/src/lib',
+    },
+  },
 })
